4b366db57ef58a622d405c6d3fd4fab84c08d067
IlonaMenkui <ilonaspolding@gmail.com>
2020-01-08 17:55:34 +0300

    code refactoring

--- index.js
+++ index.js
@@ -1,13 +1,12 @@
 const http = require('http');
 require('dotenv').config();
 
-const PORT = 3000;
 const server = http.createServer((req, res) => {
   res.statusCode = 200;
   res.setHeader('Content-Type', 'text/plain');
   res.end('Hello World\n');
 });
 
-server.listen(process.env.PORT, () => {
-  console.log(`Server running at port: ${PORT}`);
+server.listen(process.env.APP_PORT, () => {
+  console.log(`Server running at port: ${process.env.APP_PORT}`);
 });
