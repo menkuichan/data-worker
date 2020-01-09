e31e6eac8b352f8dfb8423850b6b2c230a4a0f3d
MenkuiChan <ilonaspolding@gmail.com>
2020-01-09 15:08:42 +0300

    create movie schema and save to database one simple movie (#4)
    * create movie schema and save to database one simple movie
    
    * change schema for movie, code refactoring
    
    * add db uri to env.example

--- index.js
+++ index.js
@@ -1,5 +1,48 @@
 const http = require('http');
+const mongoose = require('mongoose');
 require('dotenv').config();
+
+const { Schema } = mongoose;
+
+const movieScheme = new Schema({
+  title: String,
+  overview: String,
+  release_date: String,
+  genre_ids: [{
+    type: Number,
+  }],
+  vote_average: Number,
+  popularity: Number,
+  original_language: String,
+  vote_count: Number,
+  original_title: String,
+  poster_path: String,
+  adult: Boolean,
+  id: Number,
+  backdrop_path: String,
+  video: Boolean,
+});
+
+const Movie = mongoose.model('Movie', movieScheme);
+
+const firstMovie = new Movie({
+  title: 'Fight Club',
+});
+
+mongoose.connect(process.env.DB_URI, { useNewUrlParser: true });
+
+const db = mongoose.connection;
+
+db.on('error', (err) => {
+  console.error(err);
+});
+
+
+db.once('open', () => {
+  firstMovie.save(() => {
+    console.log(firstMovie.title);
+  });
+});
 
 const server = http.createServer((req, res) => {
   res.statusCode = 200;
