1e125d9d1f598e96e36d6ec81fd364308faab290
IlonaMenkui <ilonaspolding@gmail.com>
2020-01-09 12:49:55 +0300

    create movie schema and save to database one simple movie

--- index.js
+++ index.js
@@ -1,5 +1,36 @@
 const http = require('http');
+const mongoose = require('mongoose');
 require('dotenv').config();
+
+const { Schema } = mongoose;
+
+const movieScheme = new Schema({
+  title: String,
+  overview: String,
+  releaseDate: String,
+  genresIds: [{
+    type: String,
+  }],
+  voteAverage: Number,
+  popularity: Number,
+  originalLanguage: String,
+  voteCount: Number,
+  originalTitle: String,
+  posterPath: String,
+  totalPages: Number,
+});
+
+const Movie = mongoose.model('Movie', movieScheme);
+
+mongoose.connect(`mongodb://localhost/${process.env.APP_PORT}`, { useNewUrlParser: true });
+
+const firstMovie = new Movie({
+  title: 'Fight Clup',
+});
+
+firstMovie.save(() => {
+  console.log(firstMovie.title);
+});
 
 const server = http.createServer((req, res) => {
   res.statusCode = 200;
